8f80ce6a35651336005589b08cfd08561bb103d7
Lewis <iscavengerx@gmail.com>
2021-09-25 14:52:17 +0100

    implemented a 'display webcam' function for Links using JavaScript

---
+++ js/jswebcam.js
@@ -0,0 +1,17 @@
+function _attachWebCam() {
+  const constraints = {
+    video: true,
+    audio: true,
+  };
+  const videoElement = document.querySelector('video#localVideo');
+  async function getWebCam() {
+    try {
+      videoElement.srcObject = await navigator.mediaDevices.getUserMedia(constraints);
+    } catch(error) {
+      console.error('Error opening video camera.', error);
+    }
+  }
+  getWebCam();
+}
+
+var attachWebCam = LINKS.kify(_attachWebCam);
