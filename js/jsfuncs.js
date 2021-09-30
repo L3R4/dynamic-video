241e4db1e191f7541b9ced719e6a4c4246c98646
Lewis Raeburn <lewisraeburn1999@gmail.com>
2021-09-30 11:40:36 +0100

    Added Links code which allows clients to broadcast messages to all connected clients

---
+++ js/jsfuncs.js
@@ -0,0 +1,6 @@
+function _objToStr(obj) {
+  let str = obj._value;
+  return str;
+}
+
+var objToStr = LINKS.kify(_objToStr);
--- js/jswebcam.js
+++ js/jswebcam.js
@@ -1,9 +1,9 @@
-function _attachWebCam() {
+function _attachWebCam(vidID) {
   const constraints = {
     video: true,
     audio: true,
   };
-  const videoElement = document.querySelector('video#localVideo');
+  const videoElement = document.querySelector('video#' + vidID);
   async function getWebCam() {
     try {
       videoElement.srcObject = await navigator.mediaDevices.getUserMedia(constraints);
