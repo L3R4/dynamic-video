86b10e268fc37d5b95db25062dd1be2c607f1490
Lewis <iscavengerx@gmail.com>
2023-01-11 18:43:28 +0000

    commented out stuff

--- js/jsWebRTC.js
+++ js/jsWebRTC.js
@@ -71,8 +71,8 @@ function _getCameraReady(camId) {
   }
   const constraints = {
     video: {
-      width: {max: 4},
-      height: {max: 3},
+      width: {max: 200},
+      height: {max: 150},
       frameRate: {max: 60},
       deviceId: camId
     },
