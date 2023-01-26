0f84d57229f691fdce01b952b144144f7e9da2a6
Lewis <iscavengerx@gmail.com>
2023-01-26 20:51:36 +0000

    testing 30fps

--- js/jsWebRTC.js
+++ js/jsWebRTC.js
@@ -73,7 +73,7 @@ function _getCameraReady(camId) {
     video: {
       width: {max: 200},
       height: {max: 150},
-      frameRate: {max: 60},
+      frameRate: {max: 30},
       deviceId: camId
     },
     audio: false
